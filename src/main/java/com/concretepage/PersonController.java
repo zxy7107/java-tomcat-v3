65ea63a1f96ad064fdf500a7e032546d78275b96
zhangxy <zhangxy@Ctrip.com>
2018-02-11 17:37:15 +0800

    restful 接口
    https://www.concretepage.com/spring-4/spring-4-rest-web-service-json-example-tomcat

---
+++ src/main/java/com/concretepage/PersonController.java
@@ -0,0 +1,22 @@
+package com.concretepage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.concretepage.component.IPersonService;
+
+@RestController
+@RequestMapping("/data")
+public class PersonController {
+	@Autowired
+	private IPersonService personService;
+	
+	@RequestMapping("/person")
+	public Person getPersonDetail(@RequestParam(value = "id",required = false,
+	                                                    defaultValue = "0") Integer id) {
+		Person p = personService.getPersonDetail(id);
+		return p;
+	}
+}
